328867c7d28809dd56c6216f0727f5b5efb46f0a
Steve Steyaert <stephen.a.steyaert@gmail.com>
2025-12-07 23:23:57 -0500

    :sparkles: Created Utilities

---
+++ ctf-backend/src/utils/generateToken.js
@@ -0,0 +1,9 @@
+import jwt from "jsonwebtoken";
+
+export default function generateToken(user) {
+  return jwt.sign(
+    { id: user._id, isAdmin: user.isAdmin },
+    process.env.JWT_SECRET,
+    { expiresIn: "7d" }
+  );
+}
