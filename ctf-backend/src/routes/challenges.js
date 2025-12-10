16eb45dcc31f3584390836865d3112528043bc37
Steve Steyaert <stephen.a.steyaert@gmail.com>
2025-12-09 20:26:08 -0500

    fixed errors

--- ctf-backend/src/routes/challenges.js
+++ ctf-backend/src/routes/challenges.js
@@ -1,30 +1,11 @@
 import express from "express";
 import Challenge from "../models/Challenge.js";
-import auth from "../middleware/auth.js";
-import admin from "../middleware/admin.js";
 
 const router = express.Router();
 
-// Public – list challenges
 router.get("/", async (req, res) => {
   const challenges = await Challenge.find({}, "-flag");
   res.json(challenges);
-});
-
-// Admin CRUD
-router.post("/", auth, admin, async (req, res) => {
-  const challenge = await Challenge.create(req.body);
-  res.json(challenge);
-});
-
-router.put("/:id", auth, admin, async (req, res) => {
-  const challenge = await Challenge.findByIdAndUpdate(req.params.id, req.body, { new: true });
-  res.json(challenge);
-});
-
-router.delete("/:id", auth, admin, async (req, res) => {
-  await Challenge.findByIdAndDelete(req.params.id);
-  res.json({ message: "Deleted" });
 });
 
 export default router;
