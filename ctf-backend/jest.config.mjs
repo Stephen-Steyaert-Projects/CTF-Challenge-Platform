3e8fd74066e745c506526f19b579c8665f2909cc
Steve Steyaert <stephen.a.steyaert@gmail.com>
2025-12-09 19:58:20 -0500

    Last fix

--- ctf-backend/jest.config.mjs
+++ ctf-backend/jest.config.mjs
@@ -2,7 +2,4 @@ export default {
   testEnvironment: "node",
   setupFilesAfterEnv: ["<rootDir>/test/setup.js"],
   transform: {},
-  moduleNameMapper: {
-    "^(\\.{1,2}/.*)\\.js$": "$1.js"
-  }
 };
