0211e809650af3be268d822ea02b960f570566a8
Steve Steyaert <stephen.a.steyaert@gmail.com>
2025-12-11 09:27:47 -0500

    Import Correctly

--- ctf-frontend/src/pages/Login.jsx
+++ ctf-frontend/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import AuthForm from "../components/AuthForm.jsx";
 import api from "../api/api.js";
