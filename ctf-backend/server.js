d5b2576c1ecdd8325dbd4ddffe17cc2bfd08e45e
Steve Steyaert <stephen.a.steyaert@gmail.com>
2025-12-10 21:13:58 -0500

    Fix CORS

--- ctf-backend/server.js
+++ ctf-backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
 app.use(cookieParser());
