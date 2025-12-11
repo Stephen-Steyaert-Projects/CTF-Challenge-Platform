8b4bf2e4dc77f4fce77f596e63bf45bb6835582f
Steve Steyaert <stephen.a.steyaert@gmail.com>
2025-12-10 22:58:57 -0500

    :pencil2: Finals

--- ctf-frontend/src/api/api.js
+++ ctf-frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://backend:5000",
+  import.meta.env.VITE_PUBLIC_API_URL || "http://localhost:4000",
   withCredentials: true, // important for JWT HttpOnly cookies
 });
 
