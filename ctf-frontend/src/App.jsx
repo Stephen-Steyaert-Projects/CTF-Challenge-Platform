29fb348005127f04c3660f66bb3089c5e8db2ad9
Steve Steyaert <stephen.a.steyaert@gmail.com>
2025-12-10 20:54:14 -0500

    Updated

--- ctf-frontend/src/App.jsx
+++ ctf-frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Login from "./pages/Login.jsx";
 import Register from "./pages/Register.jsx";
@@ -6,13 +6,11 @@ import Admin from "./pages/Admin.jsx";
 
 export default function App() {
   return (
-    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/admin" element={<Admin />} />
       </Routes>
-    </Router>
   );
 }
