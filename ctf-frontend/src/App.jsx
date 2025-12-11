5f2fd40e1f97ceaefd56a77bc68bcd845947ee65
Steve Steyaert <stephen.a.steyaert@gmail.com>
2025-12-11 08:41:31 -0500

    Fix Error

--- ctf-frontend/src/App.jsx
+++ ctf-frontend/src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./pages/Home.jsx";
 import Login from "./pages/Login.jsx";
 import Register from "./pages/Register.jsx";
 import Admin from "./pages/Admin.jsx";
-import Logout from "./pages/Logout.jsx";
+import LogoutPage from "./pages/Logout.jsx";
 import Navbar from "./components/Navbar.jsx";
 
 export default function App() {
@@ -15,7 +15,7 @@ export default function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/admin" element={<Admin />} />
-        <Route path="/logout" element={<Logout />} />
+        <Route path="/logout" element={<LogoutPage />} />
       </Routes>
     </>
   );
--- ctf-frontend/src/pages/Logout.jsx
+++ ctf-frontend/src/pages/Logout.jsx
@@ -1,5 +1,5 @@
 import Logout from "../components/Logout.jsx";
 
-export default function Logout() {
+export default function LogoutPage() {
   return <Logout />;
 }
