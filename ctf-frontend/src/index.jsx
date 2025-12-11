24f7c9de4968019b0717a1d41daac5ae6c82fffb
Steve Steyaert <stephen.a.steyaert@gmail.com>
2025-12-11 09:09:18 -0500

    Fix error

--- ctf-frontend/src/index.jsx
+++ ctf-frontend/src/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { AuthProvider } from "./context/AuthContext.jsx";
+import AuthProvider from "./context/AuthContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
