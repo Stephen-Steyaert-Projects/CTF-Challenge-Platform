e14daa803bcf42073b6e1740e0ee310173f9ff08
Steve Steyaert <stephen.a.steyaert@gmail.com>
2025-12-10 21:38:35 -0500

    :pencil2: CORS Fixes

--- ctf-backend/server.js
+++ ctf-backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: "*",
+  origin: "http://localhost:3000",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
