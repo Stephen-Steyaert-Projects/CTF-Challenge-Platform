69ed4e9b7ca0ab2fd3739fe74d9b6691b3b1ebd5
Steve Steyaert <stephen.a.steyaert@gmail.com>
2025-12-10 21:06:06 -0500

    :pencil2: Fixing dumb mistake

--- ctf-frontend/src/App.jsx
+++ ctf-frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Login from "./pages/Login.jsx";
 import Register from "./pages/Register.jsx";
